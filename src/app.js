d0c2db1632ad072f80c628768da66d8159c6f2d1
Alessander Leon <alessanderleon@gmail.com>
2025-02-21 12:48:45 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+ window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+function Generador() {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let domainHacks = ['.es', '.com'];
+
+  let tex1 = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let tex2 = adj[Math.floor(Math.random() * adj.length)];
+  let tex3 = noun[Math.floor(Math.random() * noun.length)];
+  let tex4 = domainHacks[Math.floor(Math.random() * domainHacks.length)];
+  console.log(tex1 + tex2 + tex3 + tex4)
+}
+
+Generador()
+Generador()
+Generador()
+Generador()
+Generador()
+Generador()
+Generador()
+Generador()
+
 };
